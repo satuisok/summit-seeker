49076f635adf3449414457ca5faf472c8189abf5
Satu Isokaanta <satu.isokaanta@outlook.com>
2023-08-22 12:50:06 +0300

    add review post delete hook to update rock avgRating

--- models/reviews.js
+++ models/reviews.js
@@ -17,15 +17,40 @@ const ReviewSchema = new Schema({
 ReviewSchema.post('save', async function () {
     const reviewRock = await this.rock.populate('reviews', 'rating');
     const totalLength = this.rock.reviews.length;
+
     const totalRating = reviewRock.reviews.reduce(function (acc, review) {
         return acc + review.rating;
     }, 0);
-    console.log(totalRating)
-
     const newAvg = Math.round(totalRating / totalLength);
-    console.log(newAvg);
+
     this.rock.avgRating = newAvg;
     await this.rock.save();
+
+})
+
+ReviewSchema.post('findOneAndDelete', async function (review) {
+    const rockAverage = await review.populate('rock', 'avgRating');
+    const rockRating = await review.populate(
+        {
+            path: 'rock',
+            populate: {
+                path: 'reviews',
+            }
+        }
+    );
+
+    const totalLength = rockRating.rock.reviews.length;
+
+    const totalRating = rockRating.rock.reviews.reduce(function (acc, review) {
+        return acc + review.rating;
+    }, 0);
+
+    const newAvg = Math.round(totalRating / totalLength);
+    rockAverage.rock.avgRating = newAvg;
+
+    await rockAverage.rock.save();
+    console.log(rockAverage.rock.avgRating);
+    console.log(newAvg);
 
 })
 
